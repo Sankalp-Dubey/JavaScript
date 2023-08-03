f8ecd71091d8ca15b53100f4e1c886e3f32142b9
Sankalp Dubey <134829949+Sankalp-Dubey@users.noreply.github.com>
2023-08-04 00:24:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const input = document.getElementById("numOfwords");
+const container = document.querySelector(".container");
+
+const generateword = (n)=> {
+    let text = "";
+    const letter = "ABCDEFGHIJKLMNOPQRSTWXYZ";
+
+    for (let i=0; i<n ; ++i){
+        const random = (Math.random() * 25).toFixed(0);
+        text += letter[random];
+    }
+
+    return text;
+};
+
+const generatepara = ()=> {
+const numOfWords = Number(input.value);
+
+const para = document.createElement("p");
+
+let data = "";
+
+for (let i=0 ; i < numOfWords; ++i){
+    const randomNumber = (Math.random() * 15).toFixed(0);
+    data += generateword(randomNumber);
+    data += " ";
+
+}
+
+para.innerText = data;
+
+para.setAttribute("class", "paras");
+
+container.append(para);
+
+}
+
+
+
+
